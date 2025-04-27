0f566ed864a8b5b07cfa0a33193f69f84b1712e8
Jleilia Med Amin <jleliamedamin@gmail.com>
2025-04-27 14:42:47 +0100

    last update

--- src/app/admin/events/models/environment.ts
+++ src/app/admin/events/models/environment.ts
@@ -1,7 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    huggingfaceKey: '',
-    openaiKey: '',
     cohereKey: 'o7PrgDN4FkiLTsdETGdoxYaheukbBzPPgVzSYSQR'
   };
